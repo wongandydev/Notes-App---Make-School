997321a041894186b8ee9cff884abcc93b204e66
Andy Wong <wongandy97@gmail.com>
2016-06-26 20:25:53 -0400

    Import RealmHelper
    imported Realm Helpeer into a new swift file created under Helper
    Folder
    
    MakeSchoolNotes > Helpers > RealmHelper.swift

---
+++ MakeSchoolNotes/Controllers/RealmHelper.swift
@@ -0,0 +1,14 @@
+//
+//  RealmHelper.swift
+//  MakeSchoolNotes
+//
+//  Created by Andy Wong on 6/26/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import RealmSwift
+import Foundation
+
+class RealmHelper {
+    
+}
